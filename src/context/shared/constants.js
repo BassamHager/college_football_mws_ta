c2440181069dae51d88f310f0337f208cb87975f
Bassam Hager <hasa374777@exactsoftware.com>
2021-10-23 09:03:31 +0200

    create an http hook & get mocked data to prepare functionality & style beforehand

--- src/context/shared/constants.js
+++ src/context/shared/constants.js
@@ -1,1 +1,2 @@
 export const LOGIN_PASSWORD = "123123";
+export const GET_TEAMS_URL = "https://api.collegefootballdata.com/teams";
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,14 @@
-import { useContext, useEffect } from "react";
+import { useCallback, useContext, useEffect } from "react";
 import "./HomePage.css";
 import { useHistory } from "react-router-dom";
 
 // context
 import { AuthContext } from "../../context/auth/AuthContext";
+// import { GET_TEAMS_URL } from "../../context/shared/constants";
+import mocked from "../../context/mock/mockData.json";
+
+// hooks
+// import { useHttpClient } from "../../util/httpHook";
 
 const HomePage = () => {
   // history
@@ -12,10 +17,26 @@ const HomePage = () => {
   // context
   const { isAuthorized, setIsAuthorized } = useContext(AuthContext);
 
+  // hooks
+  // const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  // fetch teams
+  const fetchTeams = useCallback(async () => {
+    // const teams = await sendRequest(GET_TEAMS_URL, "GET", null, {
+    //   "Access-Control-Allow-Origin": "*",
+    //   accept: "application/json",
+    //   Authorization:
+    //     "Bearer S3jwPbEIdMnpNZfs06X90rM4jraUNbtCQ+g0t7t+pdDKqaiNVrc2eLEXdEeX5hhS",
+    // });
+    const teams = mocked;
+    console.log(teams);
+  }, []);
+
+  // fetchTeams();
   // force redirect to login page
   useEffect(() => {
-    !isAuthorized && history.push("/login");
-  }, [isAuthorized, history]);
+    !isAuthorized ? history.push("/login") : fetchTeams();
+  }, [isAuthorized, history, fetchTeams]);
 
   return (
     <div className="home">
